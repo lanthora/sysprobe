44768da78baec1584eb3445556fc6da89039f46c
lanthora <lanthora@outlook.com>
2022-12-01 14:28:56 +0000

    网络时延打印地址

--- sysprobe-ebpf/tcp.h
+++ sysprobe-ebpf/tcp.h
@@ -7,7 +7,30 @@
 
 static int trace_tcp_probe(struct trace_event_raw_tcp_probe *ctx)
 {
-	LOG("tcp_probe: srtt=%d", ctx->srtt);
+	static const int AF_INET = 2;
+	static const int AF_INET6 = 10;
+	static const int SRTT_THRESHOLD = 10000;
+
+	if (ctx->srtt < SRTT_THRESHOLD)
+		return 0;
+
+	void *saddr, *daddr;
+
+	switch (ctx->family) {
+	case AF_INET:
+		saddr = &((struct sockaddr_in *)&ctx->saddr)->sin_addr;
+		daddr = &((struct sockaddr_in *)&ctx->daddr)->sin_addr;
+		LOG("tcp_probe: src=%pI4.%u dst=%pI4.%u srtt=%d", saddr, ctx->sport, daddr, ctx->dport, ctx->srtt);
+		break;
+	case AF_INET6:
+		saddr = &((struct sockaddr_in6 *)&ctx->saddr)->sin6_addr;
+		daddr = &((struct sockaddr_in6 *)&ctx->daddr)->sin6_addr;
+		LOG("tcp_probe: src=%pI6.%u dst=%pI6.%u srtt=%d", saddr, ctx->sport, daddr, ctx->dport, ctx->srtt);
+		break;
+	default:
+		break;
+	}
+
 	return 0;
 }
 
